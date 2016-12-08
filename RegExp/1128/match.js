dc9454ba522bcae8b96d52d73bc5f673741db5c8
qianbei476 <584854762@qq.com>
2016-12-08 18:31:56 +0800

    include links

--- RegExp/1128/match.js
+++ RegExp/1128/match.js
@@ -3,8 +3,8 @@
 
 function match(body){
   var regexp = /([a-z0-9]+)@([a-z0-9]+).com/g;
+  // var regexp = /(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?/g; 链接
   var res = body.match(regexp);
-
   if(res){
     console.log('All email address number:'+res.length);
     for(var i=0;i<res.length;i++)
